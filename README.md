b250645e48f659a64f1a9ca560786aec0c9fcb2d
Silent <16026653+s-ilent@users.noreply.github.com>
2025-01-05 14:09:06 +1030

    Add script and associated files.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# udon-spawn-memory
-A simple system for respawning the player at their last position. For VRchat. 
+# Spawn Memory
+A simple system for respawning the player at their last position. For VRchat/Udon. 
+
+# Usage
+Add the package to your Packages folder, or place the script and Udon program asset into your project. 
+
+Then place the script onto a GameObject. 
+
+You can adjust these options;
+
+- **Save Interval**: How often, in seconds, the player position is saved. Due to the nature of Udon Persistence, saving the player position requires synchronising it to other users, so it is recommended to adjust this depending on network load. 
+
+- **Save Time Limit Minutes**: How long the player position should be retained for. In other words, if the player leaves the world, their saved position will be forgotten after this interval. This is useful for hangout worlds, where players who momentarily disconnect will rejoin in the same place rather than at spawn, but people who last visited the world a few days ago won't find themselves somewhere unfamiliar. 
+
+You can also see the debug values for the synched position, rotation, and last save time. 
+
+When the player rejoins the world, the script checks their last save time against the current time, and moves them to the saved position if it's within the limit. 
+
+# Notes
+This script is provided as a reference for using persistence player objects. When making it, I ran into a bunch of gotchas that the final script covers and tries to make obvious. 
+
+First, every player gets their own player object, and each one does the same script execution as any other, so scripting that only executes locally for each player needs to be gated by object ownership. 
+
+Second, `OnPlayerRestored` is fired after the player has spawned in, so they'll see themselves spawn at the regular spawn. I recommend using this in combination with a system that blanks the player's view until `OnNetworkReady` is called. 
+
---
+++ Runtime/SpawnMemory.cs
@@ -0,0 +1,95 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon.Common.Interfaces;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class SpawnMemory : UdonSharpBehaviour
+{
+    [SerializeField] private float _saveInterval = 15f;
+    [SerializeField] private int _saveTimeLimitMinutes = 20;
+    [Space(20)]
+    [SerializeField] [UdonSynced] private Vector3 _syncedPosition;
+    [SerializeField] [UdonSynced] private Quaternion _syncedRotation;
+    [SerializeField] [UdonSynced] private long _lastSaveTimeTicks;
+
+    private VRCPlayerApi _localPlayer;
+    private bool _dataRestored = false;
+    private bool _ownedByLocalPlayer = false;
+    private bool _initialized = false;
+
+    private static void DebugLog(string msg)
+    {
+        #if false
+        Debug.Log($"[SpawnMemory] {msg}");
+        #endif
+    }
+
+    public void Start()
+    {
+        DebugLog("Start.");
+        _initialized = true;
+        _localPlayer = Networking.LocalPlayer;
+        _ownedByLocalPlayer = Networking.IsOwner(_localPlayer, gameObject);
+    }
+
+    public void _UpdatePosition()
+    {
+        DebugLog("Updating position...");
+
+        if (!Utilities.IsValid(_localPlayer) || _dataRestored == false)
+        {
+            DebugLog("Invalid UpdatePosition!");
+        }
+
+        long currentTimeTicks = System.DateTime.UtcNow.Ticks;
+        Vector3 currentPosition = _localPlayer.GetPosition();
+        Quaternion currentRotation =  _localPlayer.GetRotation();
+
+        if (Mathf.Abs(_localPlayer.GetVelocity().y) > 0.5f)
+        {
+            // Do nothing.
+        }
+        else if (Vector3.Distance(_syncedPosition, currentPosition) > 0.01f) 
+        {
+            _syncedPosition = currentPosition;
+            _syncedRotation = currentRotation;
+            _lastSaveTimeTicks = currentTimeTicks;
+            RequestSerialization();
+            DebugLog($"Position should be updated to {_localPlayer.GetPosition()}, {_localPlayer.GetRotation()}");
+        }
+
+        SendCustomEventDelayedSeconds("_UpdatePosition", _saveInterval);
+    }
+
+
+    public override void OnPlayerRestored(VRCPlayerApi player)
+    {
+        DebugLog($"{gameObject.name} is restoring data for {player.displayName}...");
+        if (!Utilities.IsValid(_localPlayer))
+        {
+            _localPlayer = Networking.LocalPlayer;
+        }
+
+        if (!_initialized) DebugLog($"{gameObject.name} WARNING! Not initialized!");
+        
+        // Skip this update for this GameObject if it doesn't belong to the player. 
+        if (player != _localPlayer) return; 
+        if (!_ownedByLocalPlayer) return; 
+
+        DebugLog($"{gameObject.name} :: isLocalPlayer {player == _localPlayer} initialized {_initialized} owned by local player {_ownedByLocalPlayer} {Networking.GetOwner(this.gameObject).displayName}");
+
+        _dataRestored = true;
+
+        long currentTimeTicks = System.DateTime.UtcNow.Ticks;
+        if ((currentTimeTicks - _lastSaveTimeTicks) / System.TimeSpan.TicksPerMinute <= _saveTimeLimitMinutes 
+            && _syncedPosition != Vector3.zero)
+        {
+           _localPlayer.TeleportTo(_syncedPosition, _syncedRotation);
+           DebugLog($"Restoring {player.displayName}::{_localPlayer.displayName} to {_syncedPosition}, {_syncedRotation}, isLocal: {player.isLocal}");
+        }
+        SendCustomEventDelayedSeconds("_UpdatePosition", _saveInterval); 
+    }
+
+
+}
